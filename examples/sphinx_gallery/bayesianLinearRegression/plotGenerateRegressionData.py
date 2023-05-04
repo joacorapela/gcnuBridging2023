313014ed94c453e5bd3083b75e515b16f920e21f
Joaquin Rapela <joacorapela@gmail.com>
2023-05-04 17:54:04 +0100

    Added headers

--- examples/sphinx_gallery/bayesianLinearRegression/plotGenerateRegressionData.py
+++ examples/sphinx_gallery/bayesianLinearRegression/plotGenerateRegressionData.py
@@ -1,15 +1,39 @@
 
+"""
+Data generation script for Bayesian linear regression
+=====================================================
+
+The code below generates data for Bayesian linear regression.
+
+"""
+
+#%%
+# Import requirments
+# ------------------
+
 import numpy as np
 import plotly.graph_objects as go
+
+#%%
+# Define data generation variables
+# --------------------------------
 
 N     = 20
 a0    = -0.3
 a1    = 0.5
 sigma = 0.2
 
+#%%
+# Sample data
+# -----------
+
 x = np.random.uniform(size=N)
 y = a0 + a1 * x
 t = y + np.random.standard_normal(size=y.shape) * sigma
+
+#%%
+# Plot data
+# -----------
 
 fig = go.Figure()
 trace_y = go.Scatter(x=x, y=y, mode="lines+markers", name="true reaction times")
@@ -18,4 +42,4 @@ trace_t = go.Scatter(x=x, y=t, mode="markers", name="noisy reaction times")
 fig.add_trace(trace_t)
 fig.update_xaxes(title_text="Grating Contrast")
 fig.update_yaxes(title_text="Reaction Time")
-fig.show()
+fig
