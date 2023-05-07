214b03bc0f5d91eec58dfd2f69ef99446f2d47b9
Joaquin Rapela <joacorapela@gmail.com>
2023-05-07 15:36:52 +0100

    Saving data and figures

--- examples/sphinx_gallery/bayesianLinearRegression/plotGenerateRegressionData.py
+++ examples/sphinx_gallery/bayesianLinearRegression/plotGenerateRegressionData.py
@@ -18,18 +18,27 @@ import plotly.graph_objects as go
 # Define data generation variables
 # --------------------------------
 
-N     = 20
+n_samples = 64
 a0    = -0.3
 a1    = 0.5
 sigma = 0.2
+data_filename_pattern = "data/linearRegression_nSamples{:02d}.npz"
+fig_filename_pattern = "figures/regression_data_nSamples{:02d}.{:s}"
 
 #%%
 # Sample data
 # -----------
 
-x = np.random.uniform(size=N)
+x = np.random.uniform(size=n_samples)
 y = a0 + a1 * x
 t = y + np.random.standard_normal(size=y.shape) * sigma
+
+#%%
+# Save data
+# ---------
+
+data_filename = data_filename_pattern.format(n_samples)
+np.savez(data_filename, x=x, y=y, t=t, a0=a0, a1=a1)
 
 #%%
 # Plot data
@@ -42,4 +51,10 @@ trace_t = go.Scatter(x=x, y=t, mode="markers", name="noisy reaction times")
 fig.add_trace(trace_t)
 fig.update_xaxes(title_text="Grating Contrast")
 fig.update_yaxes(title_text="Reaction Time")
+
+png_fig_filename = fig_filename_pattern.format(n_samples, "png")
+html_fig_filename = fig_filename_pattern.format(n_samples, "html")
+fig.write_image(png_fig_filename)
+fig.write_html(html_fig_filename)
+
 fig
