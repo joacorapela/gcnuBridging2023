7f362b525067c0ed13a65cfff04141af286609ca
Joaquin Rapela <joacorapela@gmail.com>
2023-07-18 18:03:19 +0100

    Generating data in script

--- examples/sphinx_gallery/bayesianLinearRegression/plotOnlineBayesianLinearRegression.py
+++ examples/sphinx_gallery/bayesianLinearRegression/plotOnlineBayesianLinearRegression.py
@@ -24,31 +24,23 @@ import joacorapela_common.stats.bayesianLinearRegression
 # --------------------------------
 
 n_samples = 20
-n_post_samples = 6
-prior_precision_coef = 2.0
+a0 = -0.3
+a1 = 0.5
+likelihood_precision_coef = (1/0.2)**2
 n_samples_to_plot = (1, 2, 20)
-data_filename_pattern = "data/linearRegression_nSamples{:02d}.npz"
-fig_filename_pattern = ("figures/onlineBayesianLinearRegression_"
-                        "nSamples{:02d}.{:s}")
 
 #%%
-# Load data
-# ---------
+# Generate data
+# -------------
 
-data_filename = data_filename_pattern.format(n_samples)
-load_res = np.load(data_filename)
-independent_var = load_res["x"]
-noiseless_dependent_var = load_res["y"]
-dependent_var = load_res["t"]
-a0 = load_res["a0"]
-a1 = load_res["a1"]
-sigma = load_res["sigma"]
-likelihood_precision_coef = (1.0/sigma)**2
+x = np.random.uniform(low=-1, high=1, size=n_samples)
+y = a0 + a1 * x
+t = y + np.random.standard_normal(size=y.shape) * 1.0/likelihood_precision_coef
 
 #%%
-# Estimate and plot posterior
-# ---------------------------
-
+# Define plotting variables
+# -------------------------
+n_post_samples = 6
 marker_true = "cross"
 size_true = 10
 color_true = "white"
@@ -56,19 +48,28 @@ marker_data = "circle-open"
 size_data = 10
 color_data = "blue"
 line_width_data = 5
-x = np.linspace(-1, 1, 100)
-y = np.linspace(-1, 1, 100)
-X, Y = np.meshgrid(x, y)
-pos = np.dstack((X, Y))
 
-Phi = np.column_stack((np.ones(len(independent_var)), independent_var))
-alpha = prior_precision_coef
-beta = likelihood_precision_coef
+
+#%%
+# Define estimation variables
+# ---------------------------
+
+prior_precision_coef = 2.0
+
+#%%
+# Estimate and plot posterior
+# ---------------------------
+
+x_grid = np.linspace(-1, 1, 100)
+y_grid = np.linspace(-1, 1, 100)
+X_grid, Y_grid = np.meshgrid(x_grid, y_grid)
+pos = np.dstack((X_grid, Y_grid))
+
+Phi = np.column_stack((np.ones(len(x)), x))
 
 # set prior
 m0 = np.array([0.0, 0.0])
-S0 = 1.0 / alpha * np.eye(2)
-#
+S0 = 1.0 / prior_precision_coef * np.eye(2)
 
 fig = plotly.subplots.make_subplots(rows=len(n_samples_to_plot)+1, cols=3)
 x_dense = np.arange(-1.0, 1.0, 0.1)
@@ -85,7 +86,7 @@ rv = scipy.stats.multivariate_normal(m0, S0)
 
 # plot prior
 Z = rv.pdf(pos)
-trace_post = go.Contour(x=x, y=y, z=Z, showscale=False)
+trace_post = go.Contour(x=x_grid, y=y_grid, z=Z, showscale=False)
 fig.add_trace(trace_post, row=1, col=2)
 
 fig.add_trace(trace_true_coef, row=1, col=2)
@@ -108,23 +109,25 @@ fig.update_yaxes(title_text="y", row=1, col=3)
 
 mn = m0
 Sn = S0
-for n, t in enumerate(dependent_var):
-    print(f"Processing {n}/({len(dependent_var)})")
+for n, t in enumerate(y):
+    print(f"Processing {n}/({len(y)})")
     # update posterior
     mn, Sn = joacorapela_common.stats.bayesianLinearRegression.onlineUpdate(
-        mn=mn, Sn=Sn, phi=Phi[n, :], y=t, alpha=alpha, beta=beta)
+        mn=mn, Sn=Sn, phi=Phi[n, :], y=t, alpha=prior_precision_coef,
+        beta=likelihood_precision_coef)
 
     if n+1 in n_samples_to_plot:
         index_sample = n_samples_to_plot.index(n+1)
         # compute likelihood
-        Z = np.empty(shape=(len(x), len(y)), dtype=np.double)
-        for i, w0 in enumerate(x):
-            for j, w1 in enumerate(y):
-                rv = scipy.stats.norm(w0 + w1 * independent_var[n], sigma)
+        Z = np.empty(shape=(len(x_grid), len(y_grid)), dtype=np.double)
+        for i, w0 in enumerate(x_grid):
+            for j, w1 in enumerate(y_grid):
+                rv = scipy.stats.norm(w0 + w1 * x[n],
+                                      1.0/likelihood_precision_coef)
                 Z[j, i] = rv.pdf(t)
 
         # plot likelihood
-        trace_like = go.Contour(x=x, y=y, z=Z, showscale=False)
+        trace_like = go.Contour(x=x_grid, y=y_grid, z=Z, showscale=False)
         fig.add_trace(trace_like, row=index_sample+2, col=1)
 
         fig.add_trace(trace_true_coef, row=index_sample+2, col=1)
@@ -136,7 +139,7 @@ for n, t in enumerate(dependent_var):
 
         # plot updated posterior
         Z = rv.pdf(pos)
-        trace_post = go.Contour(x=x, y=y, z=Z, showscale=False)
+        trace_post = go.Contour(x=x_grid, y=y_grid, z=Z, showscale=False)
         fig.add_trace(trace_post, row=index_sample+2, col=2)
 
         fig.add_trace(trace_true_coef, row=index_sample+2, col=2)
@@ -154,7 +157,7 @@ for n, t in enumerate(dependent_var):
             trace = go.Scatter(x=x_dense, y=sample_y, mode="lines",
                                line_color="red", showlegend=False)
             fig.add_trace(trace, row=index_sample+2, col=3)
-        trace_data = go.Scatter(x=independent_var[:(n+1)], y=dependent_var[:(n+1)],
+        trace_data = go.Scatter(x=x[:(n+1)], y=y[:(n+1)],
                                 mode="markers",
                                 marker_symbol=marker_data,
                                 marker_size=size_data,
@@ -165,10 +168,5 @@ for n, t in enumerate(dependent_var):
         fig.add_trace(trace_data, row=index_sample+2, col=3)
         fig.update_xaxes(title_text="x", row=index_sample+2, col=3)
         fig.update_yaxes(title_text="y", row=index_sample+2, col=3)
-
-png_fig_filename = fig_filename_pattern.format(n_samples, "png")
-html_fig_filename = fig_filename_pattern.format(n_samples, "html")
-fig.write_image(png_fig_filename)
-fig.write_html(html_fig_filename)
 
 fig
