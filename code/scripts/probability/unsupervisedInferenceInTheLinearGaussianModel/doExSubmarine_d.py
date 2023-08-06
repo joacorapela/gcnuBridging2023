486279e604f29c85b1ea203f8499e0a32569edf5
Joaquin Rapela <joacorapela@gmail.com>
2023-08-06 18:46:51 +0100

    Changed colors and other cosmetics

--- code/scripts/probability/unsupervisedInferenceInTheLinearGaussianModel/doExSubmarine_d.py
+++ code/scripts/probability/unsupervisedInferenceInTheLinearGaussianModel/doExSubmarine_d.py
@@ -25,6 +25,12 @@ def main(argv):
     parser.add_argument("--color_measurements",
                         help="color of measurement samples",
                         type=str, default="rgba(0, 0, 255, 1.0)")
+    parser.add_argument("--color_posterior",
+                        help="color of posterior mean and PE",
+                        type=str, default="rgba(255, 165, 0, 1.0)")
+    parser.add_argument("--color_sample_mean",
+                        help="color of sample mean and its PE",
+                        type=str, default="rgba(0, 255, 0, 1.0)")
     parser.add_argument("--marker_mean",
                         help="marker for mean",
                         type=str, default="x")
@@ -37,6 +43,9 @@ def main(argv):
     parser.add_argument("--size_samples",
                         help="size for samples",
                         type=int, default=5)
+    parser.add_argument("--ylim",
+                        help="y-axes limits",
+                        type=str, default="[0.0, 4.5]")
     parser.add_argument("--y_info_filename_pattern",
                         help="pattern of filename to save samples, mean and "
                         "covariance of y", type=str,
@@ -55,10 +64,13 @@ def main(argv):
     ellipse_quantile = args.ellipse_quantile
     color_submarine = args.color_submarine
     color_measurements = args.color_measurements
+    color_posterior = args.color_posterior
+    color_sample_mean = args.color_sample_mean
     marker_mean = args.marker_mean
     marker_samples = args.marker_samples
     size_mean = args.size_mean
     size_samples = args.size_samples
+    ylim = [float(str) for str in args.ylim[1:-1].split(",")]
     y_info_filename = args.y_info_filename_pattern.format(n_samples)
     y_metadata_filename = args.y_metadata_filename_pattern.format(n_samples)
     fig_filename_pattern = args.fig_filename_pattern
@@ -89,10 +101,10 @@ def main(argv):
     #        A_sum = A1 + A2
     #     5. to multipy scalar a with matrix or vector A with numpy use
     #        aA = a * A
-    post_mean_z = ...
-    post_cov_z = ...
-    yBar_mean = ...
-    yBar_cov = ...
+    post_mean_z = x.xx
+    post_cov_z = x.xx
+    yBar_mean = x.xx
+    yBar_cov = x.xx
     #
 
     post_ellipse_x, post_ellipse_y = \
@@ -101,7 +113,7 @@ def main(argv):
             N=n_points_ellipse)
     yBar_ellipse_x, yBar_ellipse_y = \
         joacorapela_common.utils.probability.quantileEllipse(
-            mean=yBar_mean_z, cov=yBar_cov_z, quantile=ellipse_quantile,
+            mean=yBar_mean, cov=yBar_cov, quantile=ellipse_quantile,
             N=n_points_ellipse)
 
     # plot data
@@ -110,36 +122,39 @@ def main(argv):
                          marker_symbol=marker_samples,
                          marker_size=size_samples,
                          marker_color=color_submarine,
-                         name="z")
-    trace_mean = go.Scatter(x=[pos_mean_z[0]], y=[pos_mean_z[1]],
+                         name=r"$\text{submarine location}\ \mathbf{z}_1$")
+    trace_mean = go.Scatter(x=[post_mean_z[0]], y=[post_mean_z[1]],
                             mode="markers",
                             marker_symbol=marker_mean,
                             marker_size=size_mean,
-                            marker_color=color_submarine,
-                            name="posterior mean")
+                            marker_color=color_posterior,
+                            name=r"posterior mean")
     trace_post_ellipse = go.Scatter(x=post_ellipse_x, y=post_ellipse_y,
-                                    mode="lines", marker_color=color_submarine,
-                                    name="{:.0f}% posterior RE".format(
+                                    mode="lines", marker_color=color_posterior,
+                                    name="{:.0f}% posterior sample PE".format(
                                         ellipse_quantile*100))
     trace_yBar = go.Scatter(x=[sample_mean_y[0]], y=[sample_mean_y[1]],
                             mode="markers",
                             marker_symbol=marker_mean,
                             marker_size=size_mean,
-                            marker_color=color_measurements,
-                            name=r"$\bar{y}$")
+                            marker_color=color_sample_mean,
+                            name="sample mean")
     trace_yBar_ellipse = go.Scatter(x=yBar_ellipse_x, y=yBar_ellipse_y,
                                     mode="lines",
-                                    marker_color=color_measurements,
-                                    name=r"{:.0f}% $\bar{y}$ RE".format(
-                                        ellipse_quantile*100))
+                                    marker_color=color_sample_mean,
+                                    name="95% sample mean PE"
+                                   )
     fig.add_trace(trace_z)
     fig.add_trace(trace_mean)
-    fig.add_trace(trace_ellipse)
+    fig.add_trace(trace_post_ellipse)
     fig.add_trace(trace_yBar)
+    fig.add_trace(trace_yBar_ellipse)
     fig.update_layout(
         xaxis_title="x",
         yaxis_title="y",
-        yaxis={"scaleanchor": "x", "scaleratio": 1},
+        yaxis={"scaleanchor": "x", "scaleratio": 1, "range": (0, 5.5),
+               "dtick": 1.0},
+        xaxis={"range": (0, 5.5), "dtick": 1.0},
         font={"size": 18},
     )
 
@@ -149,8 +164,6 @@ def main(argv):
     html_fig_filename = fig_filename_pattern.format(n_samples, "html")
     fig.write_image(png_fig_filename)
     fig.write_html(html_fig_filename)
-
-    import pdb; pdb.set_trace()
 
 
 if __name__ == "__main__":
